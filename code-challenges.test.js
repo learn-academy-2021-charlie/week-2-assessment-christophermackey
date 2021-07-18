934444b98978b9a13f316ad539cb301bb2293156
christophermackey <c.mackey@me.com>
2021-07-18 16:19:46 -0700

    refactored question 3

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -144,7 +144,7 @@ describe("firstVowelFinder", () => {
 const firstVowelFinder = (string) => {
     for (let i = 0; i < string.length; i++) {
         if(string[i] === "a" || string[i] === "e" || string[i] === "i" || string[i] === "o" || string[i] === "u") {
-            return string.indexOf(string[i])
+            return i
         }
     }
 }
